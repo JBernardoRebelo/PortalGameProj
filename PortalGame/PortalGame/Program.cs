74ee475dff47e81c118b8780b118515f48f3a3be
JBernardoRebelo <bernasfattaker@gmail.com>
2019-08-09 12:05:16 +0100

    Organize program

--- PortalGame/PortalGame/Program.cs
+++ PortalGame/PortalGame/Program.cs
@@ -4,22 +4,56 @@ namespace PortalGame
 {
     public class Program
     {
+        // Instantiate classes
+        Render rndr = new Render();
+        GameSettings gs = new GameSettings();
+        GameManager gm = new GameManager();
+
         private static void Main(string[] args)
         {
-            // Instantiate classes
-            Render rndr = new Render();
-            GameSettings gs = new GameSettings();
-            GameManager gm = new GameManager();
+            // Instantiate program
+            Program prog = new Program();
 
             Console.WriteLine("Hellooooo"); // Debug
 
+            // Check length of args
+            prog.ArgLengthCheck(args);
+
+            // Ask new dimensions and proceed with game
+            prog.StartWithInvalidInput();
+
+            //############################################
+
+            Console.WriteLine("Number of rows: "+ prog.gs.Rows);
+            Console.WriteLine("Number of columns: "+ prog.gs.Colls);
+
+
+            //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+            // Game manager gets coordenates given
+
+        }
+
+        /// <summary>
+        /// Check console arguments' length
+        /// </summary>
+        /// <param name="args"> Accepts array of strings args</param>
+        private void ArgLengthCheck(string[] args)
+        {
             // Start through console
             if (args.Length > 4)
             {
                 // Get map dimensions from console
                 gs.DefDimmensions(args);
             }
+        }
 
+        /// <summary>
+        /// Checks if there's invalid input
+        /// If there is, ask user for new dimensions
+        /// </summary>
+        private void StartWithInvalidInput()
+        {
             // Start game through vs
             // User inserts number of rows
             if (gs.Rows <= 0)
@@ -33,17 +67,6 @@ namespace PortalGame
                 rndr.InsertDimensions(gs.Colls, "col");
                 gs.Colls = Convert.ToInt32(Console.ReadLine());
             }
-
-            //############################################
-
-            Console.WriteLine("Number of rows: "+ gs.Rows);
-            Console.WriteLine("Number of columns: "+ gs.Colls);
-
-
-            //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-            // Game manager gets coordenates given
-
         }
     }
 }
